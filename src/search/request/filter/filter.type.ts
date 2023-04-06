b705022cf22bacc3f48aec401cf5e7f4d61c3071
Jay Doerga <jaydoerga@MacBook-Pro-van-Jay.local>
2023-04-06 10:32:04 +0200

    Actual revert

--- src/search/request/filter/filter.type.ts
+++ src/search/request/filter/filter.type.ts
@@ -13,19 +13,6 @@ export type FilterType = {
   search?: StringFilter;
   // TODO: Make wildcard typing work somehow
   // eslint-disable-next-line
-  [index: string]: {
-        equals: string;
-        notEquals: string;
-        contains: string;
-        iContains: string;
-        excludes: string;
-        lt: string;
-        lte: string;
-        gt: string;
-        gte: string;
-      }
-    | StringFilter
-    | undefined
-    | Record<string, any>;
+  [index: string]: any;
   // [index: string]: StringFilter | NumericFilter | BooleanFilter | GeoFilter | DateTimeFilter | FilterType[],
 };
