5c50616f52003bdb726ffbc554e605f25af500b8
Jay Doerga <jaydoerga@MacBook-Pro-van-Jay.local>
2023-04-06 10:09:22 +0200

    Revert change filter type

--- src/search/request/filter/filter.type.ts
+++ src/search/request/filter/filter.type.ts
@@ -13,6 +13,19 @@ export type FilterType = {
   search?: StringFilter;
   // TODO: Make wildcard typing work somehow
   // eslint-disable-next-line
-  [index: string]: any;
+  [index: string]: {
+        equals: string;
+        notEquals: string;
+        contains: string;
+        iContains: string;
+        excludes: string;
+        lt: string;
+        lte: string;
+        gt: string;
+        gte: string;
+      }
+    | StringFilter
+    | undefined
+    | Record<string, any>;
   // [index: string]: StringFilter | NumericFilter | BooleanFilter | GeoFilter | DateTimeFilter | FilterType[],
 };
