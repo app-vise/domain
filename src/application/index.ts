63d3ec883c7e1679ca77a4d17b57e9cbdc77a48f
Frank Weijers <frank@weijers.io>
2023-04-19 08:58:21 +0200

    Include CountQuery export

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,3 +1,4 @@
 export { Command, CommandProps } from './command.base';
+export { CountQuery } from './count-query';
 export { ItemQuery } from './item-query';
 export { SearchQuery } from './search-query';
