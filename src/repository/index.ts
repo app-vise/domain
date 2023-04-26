c531e8119ba71e16b1f7091e587c662cf00dfbb4
Frank Weijers <frank@weijers.io>
2023-04-26 11:44:34 +0200

    Export RepositoryService

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,4 +1,5 @@
 export { ReadRepository } from './read.repository';
 export { Repository } from './repository';
+export { RepositoryService } from './repository.service';
 export { SelectionSet } from './selection-set';
 export { WriteRepository } from './write.repository';
